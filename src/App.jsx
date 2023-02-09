645afb1b2b44de805c2663f0e816cffe176d55f0
snusEnbjoer <pojiratelsnusa@gmail.com>
2023-02-09 15:20:34 +0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ let tg = window.Telegram.WebApp
 function App() {
   
   return (
-    <body>
       <div class="wrapper">
         <div class="product-img">
           <img src="https://sun9-north.userapi.com/sun9-78/s/v1/ig2/QnZ0OC_yLKyB02Qg1oJBd35r9CAiyoMEMYAQ0ocmSsHXlSeG1ivhH-M4nKlE6EzSuskkwZ8AjEqw_eSFa7p701S-.jpg?size=1200x797&quality=95&type=album" />
@@ -25,7 +24,6 @@ function App() {
           </div>
         </div>
       </div>
-    </body>
   )
 }
 
